e7795572e7486c6f5701d21e0f64512aef49d2b1
Jeff <jeffjunio.bt@gmail.com>
2024-09-08 16:18:13 -0300

    feat: improve section button and scroll

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,5 +1,8 @@
-import { it } from "node:test";
+'use client';
+
 import { Card, ICardProps } from "../Card/Card";
+import { UIEvent, useRef, useState } from "react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 interface ISectionProps {
     title: string;
@@ -8,12 +11,35 @@ interface ISectionProps {
 
 }
 export const Section = ({title, items, variant='grid'}: ISectionProps) => {
+
+    const scrollRef = useRef<HTMLUListElement>(null);
+    const [scrollAt, setScrollAt] = useState<'start' | 'middle' | 'end'>('start');
+
+    const handleScroll = (event: UIEvent<HTMLUListElement>) => {
+        console.log((event.currentTarget.scrollWidth - event.currentTarget.clientWidth) === event.currentTarget.scrollLeft);
+        
+        if (event.currentTarget.scrollLeft === 0) {
+            setScrollAt('start');
+        } else if ((event.currentTarget.scrollWidth - event.currentTarget.clientWidth) === event.currentTarget.scrollLeft) {
+            setScrollAt('end');
+        } else {
+            setScrollAt('middle');
+        }
+    };
+
+    const handleSetScroll = (scroll: number) => {
+        const currentScrollLeft = scrollRef.current?.scrollLeft || 0;
+        scrollRef.current?.scrollTo({behavior: 'smooth', left: currentScrollLeft + scroll });
+    };
+
     return (
         <section className="flex flex-col gap-4 px-4">
             <h2 className="font-bold text-xl">
                 {title}
             </h2>
             <ul 
+                onScroll={handleScroll}
+                ref={scrollRef}
                 data-variant={variant}
                 className={
                 `grid gap-2 grid-cols-1 sm:grid-cols-none
@@ -24,6 +50,17 @@ export const Section = ({title, items, variant='grid'}: ISectionProps) => {
                 `
                 }
             >
+
+                {variant === 'h-list' && (
+                    <button
+                        disabled={scrollAt === 'start'}
+                        onClick={() => handleSetScroll(-300)}  
+                        className="h-14 w-14 bg-primary rounded-full hidden sm:flex items-center justify-center sticky my-auto left-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80"  
+                    >
+                    <MdKeyboardArrowLeft size={32}/>
+                </button>
+                )}
+
                 {items.map(item => (
                     <li 
                         key={item.title} 
@@ -37,6 +74,15 @@ export const Section = ({title, items, variant='grid'}: ISectionProps) => {
                         />
                     </li>
                 ))}
+                {variant === 'h-list' && (
+                    <button
+                        disabled={scrollAt === 'end'}
+                        onClick={() => handleSetScroll(300)}  
+                        className="	h-14 w-14 bg-primary rounded-full hidden sm:flex items-center justify-center sticky my-auto right-0 -ml-14 transition-opacity disabled:opacity-0 active:opacity-80"  
+                    >
+                        <MdKeyboardArrowRight size={32}/>
+                    </button>
+                )}
             </ul>
         </section>
     );
