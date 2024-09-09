edf3fcab3f804bc0715860c467469c40b3e42792
Jeff <jeffjunio.bt@gmail.com>
2024-09-09 18:43:36 -0300

    feat: add collapsible text

--- src/components/course-header/CourseHeader.tsx
+++ src/components/course-header/CourseHeader.tsx
@@ -1,3 +1,4 @@
+import { CollapsibleText } from "./components/CollapsibleText";
 
 export const CourseHeader = () => {
 	return (
@@ -5,9 +6,9 @@ export const CourseHeader = () => {
 			<h1 className="font-extrabold text-xl">
 				Curso de Figma para DEVs
 			</h1>
-			<p>
-				Os mlehores desenvolvedores
-			</p>
+			<CollapsibleText numberOfLinesWhenClosed={3}>
+				Os melhores desenvolvedores do mercado fazem questão que estar preparados para os mais diversos tipos de desafios nas suas carreiras. A habilidade de desenvolver protótipos ou mesmo de entender como um protótipo foi desenvolvido pode ser um baita diferencial para você. Nesse curso que te mostrar de forma simples e prática como desenvolver protótipos no figma, vamos aproveitar certos conhecimentos de programação ao decorrer do curso. Tenho certeza que esse tem o potencial de ser o melhor curso de figma para desenvolvedores disponíveis gratuitamente. #CODARSE
+			</CollapsibleText>
 			
 			<div className="flex gap-2 items-center">
 				<button className="py-2 px-4 bg-paper rounded-full">
