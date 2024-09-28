3a945b0f85a3ee2eed1a27d77da8b9874f891035
Jeff <jeffjunio.bt@gmail.com>
2024-09-28 11:17:41 -0300

    feat: add start course floating button

--- src/app/cursos/[id]/page.tsx
+++ src/app/cursos/[id]/page.tsx
@@ -21,12 +21,20 @@ export default function PageCourseDetail({ params }: Props) {
 		<main className='mt-8 flex justify-center'>
 			<div className="w-full min-[880px]:max-w-[880px] p-2 flex flex-col gap-4">
 				<StartCourse 
+					title="Curso de Figma para DEVs"
 					idClass="1"
 					idCourse="1"
 					imageUrl="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
 				/>
 
                 <CourseHeader />
+				<CourseHeader />
+				<CourseHeader />
+				<CourseHeader />
+				<CourseHeader />
+				<CourseHeader />
+				<CourseHeader />
+
             </div>
 		</main>
 	);
