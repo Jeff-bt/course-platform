40f87db49f8ddf86791d658a49e5486bd299b89c
Jeff <jeffjunio.bt@gmail.com>
2024-10-04 14:43:29 -0300

    feat: fix padding bottom and course header props

--- src/components/course-header/CourseHeader.tsx
+++ src/components/course-header/CourseHeader.tsx
@@ -1,25 +1,31 @@
+'use client';
 import { MdShare } from "react-icons/md";
 import { CollapsibleText } from "./components/CollapsibleText";
 import { ContentCopy } from "./components/CopyContext";
 
-export const CourseHeader = () => {
+interface ICourseHeaderProps {
+	title: string;
+	description: string;
+	numberOfClasses: number;
+}
+export const CourseHeader = ({ title, description, numberOfClasses }: ICourseHeaderProps) => {
 	return (
 		<div className="flex flex-col gap-2">
 			<h1 className="font-extrabold text-xl">
-				Curso de Figma para DEVs
+				{title}
 			</h1>
 			<CollapsibleText numberOfLinesWhenClosed={3}>
-				Os melhores desenvolvedores do mercado fazem questão que estar preparados para os mais diversos tipos de desafios nas suas carreiras. A habilidade de desenvolver protótipos ou mesmo de entender como um protótipo foi desenvolvido pode ser um baita diferencial para você. Nesse curso que te mostrar de forma simples e prática como desenvolver protótipos no figma, vamos aproveitar certos conhecimentos de programação ao decorrer do curso. Tenho certeza que esse tem o potencial de ser o melhor curso de figma para desenvolvedores disponíveis gratuitamente. #CODARSE
+				{description}
 			</CollapsibleText>
 			
 			<div className="flex gap-2 items-center">
-				<ContentCopy title='Copie o coteúdo abaixo' content='https://www.radix-ui.com/primitives/docs/components/dropdown-menu'>
+				<ContentCopy title='Copie o coteúdo abaixo' content={window.location.href}>
 					<button className="py-2 px-4 bg-paper rounded-full flex items-center gap-2">
 						<MdShare />
 						Compartilhar
 					</button>
 				</ContentCopy>
-				<span>48 aulas</span>
+				<span>{numberOfClasses} aulas</span>
 			</div>
 		</div>
 	);
