a6f6cc894c8fbdb5cb25791208cab2cdec287b9d
Jeff <jeffjunio.bt@gmail.com>
2024-10-04 19:17:24 -0300

    feat: add class group component

--- src/app/player/[courseId]/[classId]/page.tsx
+++ src/app/player/[courseId]/[classId]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { PlayerClass } from "@/components/player/PlayerClass";
+import { PlayerClassGroup } from "@/components/player/PlayerClassGroup";
 import { PlayerHeader } from "@/components/player/PlayerHeader";
 
 interface Props {
@@ -10,19 +11,34 @@ interface Props {
 }
 export default function PagePlayer({params: { classId, courseId }}: Props) {
     return (
-        <>
+        <main className="flex flex-col gap-20">
             <PlayerHeader
                 title="Curso"
                 subtitle="descrição"
             />
-            <PlayerClass
-                done={false}
-                playing={true}
-                title="Titulo da aula"
-
-                onCheck={() => console.log("check")}
-                onPlay={() => console.log("check")}
+            <PlayerClassGroup
+                onToggle={() => console.log("toogle")}
+                open={true}
+                title="Title"
+                position={1}
+                classes={[
+                    {
+                        done: true,
+                        playing: false,
+                        title:  "Titulo da aula"
+                    },
+                    {
+                        done: false,
+                        playing: false,
+                        title:  "Titulo da aula"
+                    },
+                    {
+                        done: true,
+                        playing: true,
+                        title:  "Titulo da aula"
+                    }
+                ]}
             />
-        </>
+        </main>
     );
 };
